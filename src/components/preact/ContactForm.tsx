0b13aa1d63ea70aaf285b6db50122bede872468a
mikecode <miguelalbertoaz@gmail.com>
2025-04-19 23:21:45 +0200

    agrega formulario en la pagina de contacto

---
+++ src/components/preact/ContactForm.tsx
@@ -0,0 +1,44 @@
+import css from "@/styles/components/ContactForm.module.css";
+
+interface Props {
+  heading: string;
+}
+
+export function ContactForm({ heading }: Props) {
+  return (
+    <form class={css.Form}>
+      <h2 class="text-gradient-primary">{heading}</h2>
+      <div class={css.Field}>
+        <label class={css.Label} htmlFor="">
+          nombre
+        </label>
+        <input class={css.Input} type="text" />
+      </div>
+      <div class={css.Field}>
+        <label class={css.Label} htmlFor="">
+          email
+        </label>
+        <input class={css.Input} type="text" />
+      </div>
+      <div class={css.Field}>
+        <label class={css.Label} htmlFor="">
+          teléfono
+        </label>
+        <input class={css.Input} type="text" />
+      </div>
+      <div class={`${css.Field} ${css.TextareaField}`}>
+        <label class={css.Label} htmlFor="">
+          mensaje
+        </label>
+        <textarea class={css.Textarea} name="" id=""></textarea>
+      </div>
+      <div>
+        <label class={`${css.Label} ${css.LabelLegal}`} htmlFor="">
+          <input type="checkbox" />
+          <span>Acepto la política de privacidad</span>
+        </label>
+      </div>
+      <button class={`${css.Submit} btn`}>enviar</button>
+    </form>
+  );
+}
