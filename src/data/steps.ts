51fb686d6e3b70d809d9700da9d9555eb9e56474
mikecode <miguelalbertoaz@gmail.com>
2025-04-22 18:49:17 +0200

    agrega categorias y posts iniciales

--- src/data/steps.ts
+++ src/data/steps.ts
@@ -4,12 +4,12 @@ export const steps: Step[] = [
   {
     title: "Análisis inicial del caso",
     description:
-      "Tras el primer contacto, revisaré su situación y la documentación disponible para valorar si el caso es viable desde el punto de vista médico-legal.",
+      "Tras el primer contacto, revisaré su situación y la documentación disponible para valorar si el caso es viable desde el punto de vista médico-legal. En algunos casos será necesario la exploración física del paciente.",
   },
   {
     title: "Elaboración del informe",
     description:
-      "Si el caso es defendible, procederé a redactar el informe correspondiente, basado en criterios médicos y jurídicos objetivos.",
+      "Una vez recopilada toda la información necesaria, procederé a redactar el informe correspondiente, basado en criterios médicos y jurídicos objetivos.",
   },
   {
     title: "Entrega y acompañamiento",
