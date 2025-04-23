93dd306f3f556f86264f0d4a3c8e6b8ef2d405ed
mikecode <miguelalbertoaz@gmail.com>
2025-04-23 09:44:15 +0200

    agrega ajustas para mostrar la web de prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), mdx()],
   server: {
-    host: true
-  }
+    host: true,
+    allowedHosts: true,
+  },
 });
--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -2,25 +2,16 @@ import type { FAQ } from "@/types";
 
 export const FAQ_HOME: FAQ[] = [
   {
-    heading: "¿Qué pasa si un hermano no quiere vender la casa?",
-    text: "Si uno de tus hermanos no quiere vender la casa, tendrás que intentar un acto de conciliación, vender tu parte de la herencia o iniciar una extinción de condominio.",
+    heading: "Pregunta 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi officia laborum debitis, est fugiat! Vel error debitis laborum deleniti labore, enim ullam nihil aperiam ad fuga unde sit repellat.",
   },
   {
-    heading: "¿Se puede vender una herencia si un heredero no quiere?",
-    text: "No, para que un inmueble se pueda vender, todos los propietarios deben estar de acuerdo con esta decisión.",
+    heading: "Pregunta 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi officia laborum debitis, est fugiat! Vel error debitis laborum deleniti labore, enim ullam nihil aperiam ad fuga unde sit repellat.",
   },
   {
-    heading:
-      "¿Cómo elegir la mejor solución si en una herencia una parte no quiere vender?",
-    text: "Primero averigua cuánto vale tu parte de la herencia y luego compara la cifra respecto a lo que podrías obtener en una subasta pública, teniendo en cuenta los gastos del abogado.",
-  },
-  {
-    heading: "No quiero vender mi parte de herencia ¿Qué puedo hacer?",
-    text: "Puedes comprar las otras partes al resto de herederos para hacerte con el pleno dominio, si no cuentas con la capacidad económica, podrías llegar a un acuerdo con los demás propietarios para pagar un alquiler.",
-  },
-  {
-    heading: "¿Se puede vender un terreno si un heredero no quiere?",
-    text: "En el caso de un terreno también puede un heredero negarse a vender su parte, la diferencia es que se podría llegar a un acuerdo y dividir las tierras según la legislación.",
+    heading: "Pregunta 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi officia laborum debitis, est fugiat! Vel error debitis laborum deleniti labore, enim ullam nihil aperiam ad fuga unde sit repellat.",
   },
 ];
 
