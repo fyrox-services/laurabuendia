f4a921972430fb358bff32679a8cc4bd9e1b6b3d
mikecode <miguelalbertoaz@gmail.com>
2025-05-03 14:20:09 +0200

    agrega datos estructurados a la home y favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
+  site: "https://lauraperitomedico.com",
   server: {
     host: true,
-    allowedHosts: true,
+    // allowedHosts: true,
   },
 });
