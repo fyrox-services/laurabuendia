087f3ed6c36f96f3302416c4eebdae617ffb73d5
mikecode <miguelalbertoaz@gmail.com>
2025-05-06 18:24:31 +0200

    agrega sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,28 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
   site: "https://lauraperitomedico.com",
+  integrations: [
+    mdx(),
+    react(),
+    sitemap({
+      filter: (page) => {
+        const excludedPages = [
+          "https://lauraperitomedico.com/blog/",
+          "https://lauraperitomedico.com/contacto/",
+          "https://lauraperitomedico.com/legal/aviso-legal/",
+          "https://lauraperitomedico.com/legal/politica-cookies/",
+          "https://lauraperitomedico.com/legal/politica-privacidad/",
+        ];
+        return !excludedPages.includes(page);
+      },
+    }),
+  ],
   server: {
     host: true,
-    // allowedHosts: true,
   },
 });
