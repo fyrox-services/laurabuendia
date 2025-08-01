83f4ea87b277b7b1b6e03b433d1c1aeff3874ce2
mikecode <miguelalbertoaz@gmail.com>
2025-08-01 12:43:56 +0200

    agrega helper de image para las imágenes de las colecciones

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,36 @@
 import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  schema: z.object({
-    metaTitle: z.string(),
-    metaDescription: z.string(),
-    h1: z.string(),
-    readingTime: z.number(),
-    datePublished: z.date(),
-    dateModified: z.date(),
-    category: z.enum(["accidentes de tráfico", "peritaje médico", "negligencia médica"]),
-    image: z.string(),
-    wordCount: z.number(),
-    description: z.string(),
-    author: z.enum(["Laura Buendía"]),
-    topPick: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      metaTitle: z.string(),
+      metaDescription: z.string(),
+      h1: z.string(),
+      readingTime: z.number(),
+      datePublished: z.date(),
+      dateModified: z.date(),
+      category: z.enum([
+        "accidentes de tráfico",
+        "peritaje médico",
+        "negligencia médica",
+      ]),
+      image: image(),
+      wordCount: z.number(),
+      description: z.string(),
+      author: z.enum(["Laura Buendía"]),
+      topPick: z.boolean().optional(),
+    }),
 });
 
 const services = defineCollection({
-  schema: z.object({
-    metaTitle: z.string(),
-    metaDescription: z.string(),
-    h1: z.string(),
-    serviceType: z.string(),
-    image: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      metaTitle: z.string(),
+      metaDescription: z.string(),
+      h1: z.string(),
+      serviceType: z.string(),
+      image: z.string(),
+    }),
 });
 
 const categories = defineCollection({
